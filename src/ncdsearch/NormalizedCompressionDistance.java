d5a95e84d6999ef116fd0469d9dfa1678dc34df8
Takashi Ishio <ishio@is.naist.jp>
2017-11-02 12:28:01 +0900

    Added javadoc

--- src/ncdsearch/NormalizedCompressionDistance.java
+++ src/ncdsearch/NormalizedCompressionDistance.java
@@ -17,6 +17,11 @@ public class NormalizedCompressionDistance implements AutoCloseable {
 	private TokenSequence query;
 	private long baseSize;
 	
+	/**
+	 * Create a distance computation object.
+	 * A client must close the object to release an internal resource.   
+	 * @param query specifies a preset query.  The query is reused for multiple ncd method calls.
+	 */
 	public NormalizedCompressionDistance(TokenSequence query) {
 		deflater = new Deflater();
 		sizeRecorder = new DeflateSize();
@@ -24,17 +29,30 @@ public class NormalizedCompressionDistance implements AutoCloseable {
 		baseSize = getCompressedDataSize(query, null);
 	}
 	
+	/**
+	 * Release an internal resource.
+	 */
 	@Override
 	public void close() {
 		deflater.end();
 	}
 	
+	/**
+	 * Compute a distance between a preset query and a given target file. 
+	 */
     public double ncd(TokenSequence target) {
         long c2 = getCompressedDataSize(target, null);
         long c1and2 = getCompressedDataSize(query, target);
         return (c1and2 - Math.min(baseSize, c2)) * 1.0 / Math.max(baseSize, c2);
     }
 
+    /**
+     * Compute a distance between given two files.
+     * This method does not use a preset query.
+     * @param s1
+     * @param s2
+     * @return
+     */
     public long getCompressedDataSize(TokenSequence s1, TokenSequence s2) {
     	
     	deflater.reset();

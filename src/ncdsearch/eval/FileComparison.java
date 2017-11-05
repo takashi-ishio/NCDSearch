8aaa1fd58c7e4e948ef129e0b7cb2969dba2d770
Takashi Ishio <ishio@acm.org>
2017-11-05 13:57:08 +0900

    Added a command line option to compute the best match.

--- src/ncdsearch/eval/FileComparison.java
+++ src/ncdsearch/eval/FileComparison.java
@@ -18,14 +18,39 @@ public class FileComparison {
 
 	public static void main(String[] args) {
 		if (args.length < 2) {
-			System.err.println("Usage: FileComparison filename1 filename2");
+			System.err.println("Usage: FileComparison [-best] queryfile comparedfile");
 			return;
 		}
-		TokenSequence tokens1 = read(args[0]);
-		TokenSequence tokens2 = read(args[1]);
+		if (args[0].equals("-best")) {
+			System.out.println(searchBestMatch(args[1], args[2]));
+		} else {
+			System.out.println(compareSimply(args[0], args[1]));
+		}
+	}
+	
+	public static double compareSimply(String query, String target) {
+		TokenSequence tokens1 = read(query);
+		TokenSequence tokens2 = read(target);
 
 		try (NormalizedCompressionDistance ncd = new NormalizedCompressionDistance(tokens1)) {
-			System.out.println(ncd.ncd(tokens2));
+			return ncd.ncd(tokens2);
+		}
+	}
+	
+	public static double searchBestMatch(String query, String target) {
+		TokenSequence queryTokens = read(query);
+		TokenSequence tokens = read(target);
+
+		try (NormalizedCompressionDistance ncd = new NormalizedCompressionDistance(queryTokens)) {
+			double min = Double.MAX_VALUE;
+			for (int i=0; i<tokens.size(); i++) {
+				for (int j=i+1; j<=tokens.size(); j++) {
+					TokenSequence sub = tokens.substring(i, j);
+					double value = ncd.ncd(sub);
+					if (value < min) min = value;
+				}
+			}
+			return min;
 		}
 	}
 	

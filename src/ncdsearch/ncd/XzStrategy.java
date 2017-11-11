29bf9af5ded7e060429712079d847005a8e5d40f
Takashi Ishio <ishio@acm.org>
2017-11-11 13:50:47 -0800

    Fixed: implementation could not compress multiple buffers.

--- src/ncdsearch/ncd/XzStrategy.java
+++ src/ncdsearch/ncd/XzStrategy.java
@@ -3,31 +3,26 @@ package ncdsearch.ncd;
 import java.io.IOException;
 
 import org.tukaani.xz.LZMA2Options;
-import org.tukaani.xz.UnsupportedOptionsException;
 import org.tukaani.xz.XZOutputStream;
 
 public class XzStrategy implements ICompressionStrategy {
 
 	private DataSizeRecordStream sizeRecorder;
 	private LZMA2Options xzOptions;
-	private XZOutputStream stream;
+	
 	
 	public XzStrategy() {
 		sizeRecorder = new DataSizeRecordStream();
 		xzOptions = new LZMA2Options();
-		try {
-			stream = new XZOutputStream(sizeRecorder, xzOptions);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 	
 	@Override
 	public long getDataSize(byte[] buf, int start, int length) {
 		sizeRecorder.reset();
 		try {
+			XZOutputStream stream = new XZOutputStream(sizeRecorder, xzOptions);
 			stream.write(buf, start, length);
-			stream.finish();
+			stream.close();
 			return sizeRecorder.size();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -37,10 +32,6 @@ public class XzStrategy implements ICompressionStrategy {
 	
 	@Override
 	public void close() {
-		try {
-			stream.close();
-		} catch (IOException e){ 
-		}
 	}
 	
 }

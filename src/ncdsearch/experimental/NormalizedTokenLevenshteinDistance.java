6e3e3fbc4926fbafb17f69e6c8949dde16852015
Takashi Ishio <ishio@is.naist.jp>
2018-06-13 11:56:46 +0900

    Added a normalized version of Token-level LD

---
+++ src/ncdsearch/experimental/NormalizedTokenLevenshteinDistance.java
@@ -0,0 +1,25 @@
+package ncdsearch.experimental;
+
+import ncdsearch.ICodeDistanceStrategy;
+import ncdsearch.TokenSequence;
+
+public class NormalizedTokenLevenshteinDistance extends TokenLevenshteinDistance implements ICodeDistanceStrategy {
+
+	private int queryLength;
+	
+	public NormalizedTokenLevenshteinDistance(TokenSequence query) {
+		super(query);
+		this.queryLength = query.size();
+	}
+	
+	@Override
+	public double computeDistance(TokenSequence code) {
+		double d = super.computeDistance(code);
+		return d * 1.0 / Math.max(queryLength, code.size());
+	}
+	
+	@Override
+	public void close() {
+		// This object has no system resource. 
+	}
+}

7c5b6012cfc3ef21fa039ead3162f301aa34bc28
Takashi Ishio <ishio@acm.org>
2018-08-29 16:30:44 +0900

    Added token-level Levenshtein Distance mode

--- src/ncdsearch/eval/FileComparison.java
+++ src/ncdsearch/eval/FileComparison.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 
+import ncdsearch.ICodeDistanceStrategy;
 import ncdsearch.NormalizedCompressionDistance;
 import ncdsearch.SearchMain;
 import ncdsearch.TokenSequence;
+import ncdsearch.experimental.NormalizedTokenLevenshteinDistance;
 import ncdsearch.ncd.Compressor;
 import sarf.lexer.FileType;
 import sarf.lexer.TokenReader;
@@ -22,6 +24,7 @@ public class FileComparison {
 	public static void main(String[] args) {
 		int idx = 0;
 		boolean searchBest = false;
+		boolean useNormalizedTokenLD = false;
 		ArrayList<File> files = new ArrayList<>();
 		Compressor compressor = null;
 		while (idx < args.length) {
@@ -33,6 +36,9 @@ public class FileComparison {
 				if (idx < args.length) {
 					compressor = Compressor.valueOf(args[idx++].toUpperCase());
 				}
+			} else if (args[idx].equals("-ntld")) {
+				useNormalizedTokenLD = true;
+				idx++;
 			} else {
 				files.add(new File(args[idx++]));
 			}
@@ -43,23 +49,28 @@ public class FileComparison {
 			!files.get(0).canRead() ||
 			!files.get(1).isFile() ||
 			!files.get(1).canRead()) {
-			System.err.println("Usage: FileComparison [-best] [-compressor (ZIP|XZ|ZSTD)] queryfile comparedfile");
+			System.err.println("Usage: FileComparison [-best] [-compressor (ZIP|XZ|ZSTD)] [-ntld] queryfile comparedfile");
 			return;
 		}
 
+		File query = files.get(0);
+		File target = files.get(1);
 		if (searchBest) {
-			System.out.println(searchBestMatch(files.get(0), files.get(1), compressor));
+			System.out.println(searchBestMatch(query, target, compressor));
 		} else {
-			System.out.println(compareSimply(files.get(0), files.get(1), compressor));
-		}
-	}
-	
-	public static double compareSimply(File query, File target, Compressor c) {
-		TokenSequence tokens1 = read(query);
-		TokenSequence tokens2 = read(target);
-
-		try (NormalizedCompressionDistance ncd = new NormalizedCompressionDistance(tokens1, Compressor.createInstance(c))) {
-			return ncd.computeDistance(tokens2);
+			TokenSequence tokens1 = read(query);
+			TokenSequence tokens2 = read(target);
+			
+			ICodeDistanceStrategy strategy = null;
+			if (useNormalizedTokenLD) {
+				strategy = new NormalizedTokenLevenshteinDistance(tokens1);
+			} else {			
+				strategy = new NormalizedCompressionDistance(tokens1, Compressor.createInstance(compressor));
+			}
+			
+			double distance = strategy.computeDistance(tokens2);
+			strategy.close();
+			System.out.println(distance);
 		}
 	}
 	

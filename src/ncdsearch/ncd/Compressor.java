f9b71138d85e99b58e0656dd46bbbee152848899
Takashi Ishio <ishio@is.naist.jp>
2018-10-03 11:26:16 +0900

    Supported DEFLATE name.

--- src/ncdsearch/ncd/Compressor.java
+++ src/ncdsearch/ncd/Compressor.java
@@ -4,12 +4,14 @@ import java.util.zip.Deflater;
 
 public enum Compressor {
 
-	ZIP, XZ, ZSTD, FOLCA, BZIP2, SNAPPY, ZIPHIGH;
+	ZIP, DEFLATE, XZ, ZSTD, FOLCA, BZIP2, SNAPPY, ZIPHIGH;
 	
 	public static ICompressionStrategy createInstance(Compressor c) {
 		if (c != null) {
 			switch (c) {
-			case ZIP: return new DeflateStrategy();
+			case ZIP: 
+			case DEFLATE: 
+				return new DeflateStrategy();
 			case ZIPHIGH: return new DeflateStrategy(Deflater.BEST_COMPRESSION);
 			case XZ: return new XzStrategy();
 			case ZSTD: return new ZstdStrategy();

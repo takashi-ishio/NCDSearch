3f3253b177c2458e82c7e71ab8950eca39caf544
Takashi Ishio <ishio@is.naist.jp>
2019-11-11 21:58:52 +0900

    Fixed NPE

--- src/ncdsearch/report/JsonReport.java
+++ src/ncdsearch/report/JsonReport.java
@@ -1,6 +1,5 @@
 package ncdsearch.report;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
@@ -15,14 +14,14 @@ import ncdsearch.SearchConfiguration;
 public class JsonReport implements IReport {
 
 	private long startTime = System.currentTimeMillis();
-	private boolean firstFragment = true;
+	private boolean fragmentsGenerated = false;
 	private JsonGenerator gen;
 	private SearchConfiguration config;
 	
 	public JsonReport(SearchConfiguration config, OutputStream w) throws IOException {
 	    this.config = config;
 		JsonFactory factory = new JsonFactory();
-	    JsonGenerator gen = factory.createGenerator(w, JsonEncoding.UTF8);
+	    gen = factory.createGenerator(w, JsonEncoding.UTF8);
 	    gen.useDefaultPrettyPrinter();
 	    gen.writeStartObject();
 	}
@@ -34,19 +33,20 @@ public class JsonReport implements IReport {
 	
 	@Override
 	public void write(List<Fragment> fragments) throws IOException {
-		if (firstFragment) {
-			gen.writeArrayFieldStart("fragments"); 
+		if (!fragmentsGenerated) {
+			gen.writeArrayFieldStart("Result");
+			fragmentsGenerated = true;
 		}
 		for (Fragment fragment: fragments) {
 			gen.writeStartObject();
-			gen.writeStringField("filename", fragment.getFilename());
-			gen.writeNumberField("startline", fragment.getStartLine());
-			gen.writeNumberField("endline", fragment.getEndLine());
-			gen.writeNumberField("distance", fragment.getDistance());
+			gen.writeStringField("FileName", fragment.getFilename());
+			gen.writeNumberField("StartLine", fragment.getStartLine());
+			gen.writeNumberField("EndLine", fragment.getEndLine());
+			gen.writeNumberField("Distance", fragment.getDistance());
 			if (config.reportPositionDetail()) {
-				gen.writeNumberField("startchar", fragment.getStartCharPositionInLine());
-				gen.writeNumberField("endchar", fragment.getEndCharPositionInLine());
-				gen.writeStringField("tokens", fragment.getTokenString());
+				gen.writeNumberField("StartChar", fragment.getStartCharPositionInLine());
+				gen.writeNumberField("EndChar", fragment.getEndCharPositionInLine());
+				gen.writeStringField("Tokens", fragment.getTokenString());
 			}
 			gen.writeEndObject();
 		}
@@ -54,9 +54,11 @@ public class JsonReport implements IReport {
 	
 	@Override
 	public void close() throws IOException {
-		gen.writeEndArray();
+		if (fragmentsGenerated) {
+			gen.writeEndArray();
+		}
 		long time = System.currentTimeMillis() - startTime;
-		gen.writeNumberField("time-ms", time);
+		gen.writeNumberField("Time", time);
 		gen.writeEndObject();
 		gen.close();
 	}

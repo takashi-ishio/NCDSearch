42e7b1420d38d6e88b30876738af61be103c39a3
Takashi Ishio <ishio@is.naist.jp>
2019-11-27 15:08:18 +0900

    Supported parameter

--- src/ncdsearch/experimental/PredictionFilter.java
+++ src/ncdsearch/experimental/PredictionFilter.java
@@ -6,15 +6,37 @@ import ncdsearch.TokenSequence;
 public class PredictionFilter {
 
 	private TLongArrayList queryNgrams;
+	private int N;
+	private double threshold;
 	
 	public PredictionFilter(String key) {
+		// Set default values
+		N = 5;
+		threshold = 0.5;
+		// Try to parse a given parameter
+		try {
+			String[] tokens = key.split(",");
+			if (tokens.length > 0) {
+				int param = Integer.parseInt(tokens[0]);
+				if (0 < param && param < 8) {
+					 N = param;
+				}
+
+				if (tokens.length > 1) {
+					double th = Double.parseDouble(tokens[1]);
+					if (0 <= th && th <= 1) {
+						 threshold = th;
+					}
+				}
+			}
+		} catch (NumberFormatException e) {
+		}
 	}
 	
 	public void setQuery(TokenSequence query) {
-		queryNgrams = NgramDistance.createNgram(query.toByteArray(), 5);
+		queryNgrams = NgramDistance.createNgram(query.toByteArray(), N);
 	}
 	
-	private static final int N = 5; 
 	public boolean shouldSearch(TokenSequence file) {
 		try {
 			byte[] buf = file.toByteArray();
@@ -22,7 +44,7 @@ public class PredictionFilter {
 				TLongArrayList fileNgrams = NgramDistance.createNgram(buf, N);
 				int intersection = NgramDistance.computeIntersection(queryNgrams, fileNgrams);
 				double overlap = intersection * 1.0 / queryNgrams.size();
-				return overlap >= 0.5;
+				return overlap >= threshold;
 			} else {
 				return false;
 			}

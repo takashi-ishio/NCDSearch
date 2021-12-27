3b142a0d8d8c3e5cf8bd23843f65efbdb3f03f2e
Takashi Ishio <ishio@is.naist.jp>
2021-12-27 12:34:13 +0900

    Added Javadoc

--- src/ncdsearch/report/IReport.java
+++ src/ncdsearch/report/IReport.java
@@ -6,14 +6,27 @@ import java.util.List;
 
 import ncdsearch.Fragment;
 
+/**
+ * This interface defines methods to write a runtime configuration and 
+ * execution results in a particular format. 
+ */
 public interface IReport extends Closeable {
 	
+	/**
+	 * This method is called to write a configuration parameter.
+	 * @param attrName is a parameter name.
+	 * @param attrValue is a parameter value.
+	 * @throws IOException
+	 */
 	public void writeConfig(String attrName, String attrValue) throws IOException;
 
 	/**
+	 * This method is called to write a list of code fragments 
+	 * detected by the search engine.
 	 * Implementation should be thread safe because multiple threads 
-	 * would report code fragments. 
-	 * @param fragments
+	 * would report code fragments in different files. 
+	 * @param fragments is a list of detected code fragments.
+	 * @throws IOException
 	 */
 	public void write(List<Fragment> fragments) throws IOException;
 }
